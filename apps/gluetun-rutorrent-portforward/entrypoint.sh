97e8512637c33c4a5bd8cc7fb1b20e640870173f
Syphiel <Syphiel@users.noreply.github.com>
2026-03-14 19:01:30 -0400

    feat(gluetun-rutorrent-portfoward): add gluetun-rutorrent-portforward

---
+++ apps/gluetun-rutorrent-portforward/entrypoint.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+while true; do
+	if [ -f /tmp/gluetun/forwarded_port ]; then
+		FORWARDED_PORT=$(cat /tmp/gluetun/forwarded_port)
+		if [ -n "$FORWARDED_PORT" ]; then
+			echo "Port forwarded: $FORWARDED_PORT"
+			echo "RT_PORT=\'$FORWARDED_PORT\'" >/data/config.env
+			break
+		else
+			echo "Forwarded port file is empty, waiting..."
+		fi
+	else
+		echo "No port forwarded yet, waiting..."
+	fi
+	sleep 5
+done
